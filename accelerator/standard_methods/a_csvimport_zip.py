68d8c07184705ec1c220e793b03ad0e6a81d3e5e
Carl Drougge <bearded@longhaired.org>
2020-04-21 13:18:47 +0000

    fu [csvimport_zip] Preserve original filename in ds.filename.

--- accelerator/standard_methods/a_csvimport_zip.py
+++ accelerator/standard_methods/a_csvimport_zip.py
@@ -1,6 +1,6 @@
 ############################################################################
 #                                                                          #
-# Copyright (c) 2019 Carl Drougge                                          #
+# Copyright (c) 2019-2020 Carl Drougge                                     #
 # Modifications copyright (c) 2020 Anders Berkeman                         #
 #                                                                          #
 # Licensed under the Apache License, Version 2.0 (the "License");          #
